5ee2a481e6425fe6ec29919da6925f03d0e1d6c1
Anuj Gupta <satyam2465@gmail.com>
2024-09-14 16:49:57 +0530

    code

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -5,23 +5,50 @@ import java.util.Scanner;
 
 public class SecondLargest {
 
-    public static void secondLargest(int [] arr){
-        int flarge=1, slarge=-1;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>flarge && arr[i]!= flarge){
-                slarge= flarge;
-                flarge= arr[i];
-                
-            }
-            else if(flarge>arr[i] && slarge<arr[i]){
-                    slarge = arr[i];
-            }
-        }
+    static private void secondSmallest(int[] arr, int n)
+{
+	// if (n < 2)
+	// {
+	// 	return -1;
+	// }
+	int small = Integer.MAX_VALUE;
+	int second_small = Integer.MAX_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+	   if (arr[i] < small)
+	   {
+		  second_small = small;
+		  small = arr[i];
+	   }
+	   else if (arr[i] < second_small && arr[i] != small)
+	   {
+		  second_small = arr[i];
+	   }
+	}
+   System.out.println(second_small);
+}
+static private void secondLargest(int[] arr, int n)
+{
+	
+	int large = Integer.MIN_VALUE;
+	int second_large = Integer.MIN_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] > large)
+		{
+			second_large = large;
+			large = arr[i];
+		}
 
-        System.out.println(slarge);
-
-
-    }
+		else if (arr[i] > second_large && arr[i] != large)
+		{
+			second_large = arr[i];
+		}
+	}
+	System.out.println(second_large);
+}
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in); // Corrected Scanner initialization
         System.out.print("Enter the size of the array: ");
@@ -33,7 +60,8 @@ public class SecondLargest {
             arr[i] = sc.nextInt(); // Reading array elements
         }
 
-            secondLargest(arr);
+            secondLargest(arr,5);
+            secondSmallest(arr,5);
             sc.close();
     }
     
