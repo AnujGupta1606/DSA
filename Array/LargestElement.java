0b800f21395b51bc417216ad3114fa0100496cf4
Anuj Gupta <satyam2465@gmail.com>
2024-09-14 15:07:05 +0530

    Second commit

---
+++ Array/LargestElement.java
@@ -0,0 +1,33 @@
+package Array;
+import java.util.Scanner;
+
+
+
+public class LargestElement {
+    public static void Lg(int[] arr){
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max= arr[i];
+
+            }
+        }
+        System.out.println("Largest Element: "+ max);   //Time complexity O(n)
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); // Corrected Scanner initialization
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt(); // Reading array elements
+        }
+
+        Lg(arr);
+        sc.close();
+
+
+    }
+}
